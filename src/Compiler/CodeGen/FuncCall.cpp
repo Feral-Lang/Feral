9c3c7f7bbdee921ff0883cfcc0660667c6ec0e82
Chirag-Khandelwal <github@mail.chiragkh.me>
2020-02-18 08:34:39 +0530

    wrote bytecode generation for func call args

--- src/Compiler/CodeGen/FuncCall.cpp
+++ src/Compiler/CodeGen/FuncCall.cpp
@@ -11,10 +11,21 @@
 
 bool stmt_fn_call_args_t::gen_code( bcode_t & bc, const bool f1, const bool f2 ) const
 {
+	for( auto assn_arg = m_assn_args.rbegin(); assn_arg != m_assn_args.rend(); ++assn_arg ) {
+		( * assn_arg )->gen_code( bc );
+	}
+	for( auto arg = m_args.rbegin(); arg != m_args.rend(); ++arg ) {
+		( * arg )->gen_code( bc );
+	}
+
+	bc.addsz( idx(), OP_LOAD, m_assn_args.size() );
+	bc.addsz( idx(), OP_LOAD, m_args.size() );
 	return true;
 }
 
 bool stmt_fn_assn_arg_t::gen_code( bcode_t & bc, const bool f1, const bool f2 ) const
 {
+	m_rhs->gen_code( bc );
+	m_lhs->gen_code( bc );
 	return true;
 }
