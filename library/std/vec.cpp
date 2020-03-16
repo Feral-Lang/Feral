de70e7267e19f16952f2d4c0e1c80eb90bda9df9
Electrux <ElectruxRedsworth@gmail.com>
2020-03-16 16:09:52 +0530

    added various string and vector related functions in library/str.cpp and library/vec.cpp respectively

--- library/std/vec.cpp
+++ library/std/vec.cpp
@@ -85,6 +85,110 @@ var_base_t * vec_each( vm_state_t & vm, const fn_data_t & fd )
 	return make< var_vec_iterable_t >( VEC( fd.args[ 0 ] ) );
 }
 
+var_base_t * vec_front( vm_state_t & vm, const fn_data_t & fd )
+{
+	std::vector< var_base_t * > & vec = VEC( fd.args[ 0 ] )->get();
+	return vec.size() == 0 ? vm.nil : vec.front();
+}
+
+var_base_t * vec_back( vm_state_t & vm, const fn_data_t & fd )
+{
+	std::vector< var_base_t * > & vec = VEC( fd.args[ 0 ] )->get();
+	return vec.size() == 0 ? vm.nil : vec.back();
+}
+
+var_base_t * vec_push( vm_state_t & vm, const fn_data_t & fd )
+{
+	std::vector< var_base_t * > & vec = VEC( fd.args[ 0 ] )->get();
+	var_iref( fd.args[ 1 ] );
+	vec.push_back( fd.args[ 1 ] );
+	return fd.args[ 0 ];
+}
+
+var_base_t * vec_pop( vm_state_t & vm, const fn_data_t & fd )
+{
+	std::vector< var_base_t * > & vec = VEC( fd.args[ 0 ] )->get();
+	if( vec.size() > 0 ) {
+		var_dref( vec.back() );
+		vec.pop_back();
+	}
+	return fd.args[ 0 ];
+}
+
+var_base_t * vec_setat( vm_state_t & vm, const fn_data_t & fd )
+{
+	srcfile_t * src_file = vm.src_stack.back()->src();
+	if( fd.args[ 1 ]->type() != VT_INT ) {
+		src_file->fail( fd.idx, "expected first argument to be of type integer for vec.set(), found: %s",
+				vm.type_name( fd.args[ 1 ]->type() ).c_str() );
+		return nullptr;
+	}
+	size_t pos = INT( fd.args[ 1 ] )->get().get_ui();
+	std::vector< var_base_t * > & vec = VEC( fd.args[ 0 ] )->get();
+	if( pos >= vec.size() ) {
+		src_file->fail( fd.idx, "position %zu is not within string of length %zu",
+				pos, vec.size() );
+		return nullptr;
+	}
+	var_dref( vec[ pos ] );
+	var_iref( fd.args[ 2 ] );
+	vec[ pos ] = fd.args[ 2 ];
+	return fd.args[ 0 ];
+}
+
+var_base_t * vec_insert( vm_state_t & vm, const fn_data_t & fd )
+{
+	srcfile_t * src_file = vm.src_stack.back()->src();
+	if( fd.args[ 1 ]->type() != VT_INT ) {
+		src_file->fail( fd.idx, "expected first argument to be of type integer for string.insert(), found: %s",
+				vm.type_name( fd.args[ 1 ]->type() ).c_str() );
+		return nullptr;
+	}
+	size_t pos = INT( fd.args[ 1 ] )->get().get_ui();
+	std::vector< var_base_t * > & vec = VEC( fd.args[ 0 ] )->get();
+	if( pos > vec.size() ) {
+		src_file->fail( fd.idx, "position %zu is greater than vector length %zu",
+				pos, vec.size() );
+		return nullptr;
+	}
+	var_iref( fd.args[ 2 ] );
+	vec.insert( vec.begin() + pos, fd.args[ 2 ] );
+	return fd.args[ 0 ];
+}
+
+var_base_t * vec_erase( vm_state_t & vm, const fn_data_t & fd )
+{
+	srcfile_t * src_file = vm.src_stack.back()->src();
+	if( fd.args[ 1 ]->type() != VT_INT ) {
+		src_file->fail( fd.idx, "expected argument to be of type integer for string.erase(), found: %s",
+				vm.type_name( fd.args[ 1 ]->type() ).c_str() );
+		return nullptr;
+	}
+	size_t pos = INT( fd.args[ 1 ] )->get().get_ui();
+	std::vector< var_base_t * > & vec = VEC( fd.args[ 0 ] )->get();
+	if( pos < vec.size() ) vec.erase( vec.begin() + pos );
+	return fd.args[ 0 ];
+}
+
+var_base_t * vec_last( vm_state_t & vm, const fn_data_t & fd )
+{
+	return make< var_int_t >( VEC( fd.args[ 0 ] )->get().size() - 1 );
+}
+
+var_base_t * vec_at( vm_state_t & vm, const fn_data_t & fd )
+{
+	srcfile_t * src_file = vm.src_stack.back()->src();
+	if( fd.args[ 1 ]->type() != VT_INT ) {
+		src_file->fail( fd.idx, "expected argument to be of type integer for string.erase(), found: %s",
+				vm.type_name( fd.args[ 1 ]->type() ).c_str() );
+		return nullptr;
+	}
+	std::vector< var_base_t * > & vec = VEC( fd.args[ 0 ] )->get();
+	size_t pos = INT( fd.args[ 1 ] )->get().get_ui();
+	if( pos >= vec.size() ) return vm.nil;
+	return vec[ pos ];
+}
+
 var_base_t * vec_iterable_next( vm_state_t & vm, const fn_data_t & fd )
 {
 	var_vec_iterable_t * it = VEC_ITERABLE( fd.args[ 0 ] );
@@ -102,6 +206,16 @@ REGISTER_MODULE( vec )
 
 	vm.add_typefn( VT_VEC,   "len", new var_fn_t( src_name, {}, {}, { .native = vec_size  }, 0, 0 ), false );
 	vm.add_typefn( VT_VEC, "empty", new var_fn_t( src_name, {}, {}, { .native = vec_empty }, 0, 0 ), false );
+	vm.add_typefn( VT_VEC, "front", new var_fn_t( src_name, "",  "", {}, {}, { .native = vec_front }, true, 0, 0 ), false );
+	vm.add_typefn( VT_VEC,  "back", new var_fn_t( src_name, "",  "", {}, {}, { .native = vec_back  }, true, 0, 0 ), false );
+	vm.add_typefn( VT_VEC,  "push", new var_fn_t( src_name, "",  "", { "" }, {}, { .native = vec_push }, true, 0, 0 ), false );
+	vm.add_typefn( VT_VEC,   "pop", new var_fn_t( src_name, "",  "", {}, {}, { .native = vec_pop   }, true, 0, 0 ), false );
+	vm.add_typefn( VT_VEC, "insert", new var_fn_t( src_name, "",  "", { "", "" }, {}, { .native = vec_insert }, true, 0, 0 ), false );
+	vm.add_typefn( VT_VEC, "erase", new var_fn_t( src_name, "",  "", { "" }, {}, { .native = vec_erase }, true, 0, 0 ), false );
+	vm.add_typefn( VT_VEC, "lastidx", new var_fn_t( src_name, "",  "", {}, {}, { .native = vec_last }, true, 0, 0 ), false );
+	vm.add_typefn( VT_VEC,   "set", new var_fn_t( src_name, "",  "", { "", "" }, {}, { .native = vec_setat }, true, 0, 0 ), false );
+	vm.add_typefn( VT_VEC,    "at", new var_fn_t( src_name, "",  "", { "" }, {}, { .native = vec_at }, true, 0, 0 ), false );
+	vm.add_typefn( VT_VEC,    "[]", new var_fn_t( src_name, "",  "", { "" }, {}, { .native = vec_at }, true, 0, 0 ), false );
 	vm.add_typefn( VT_VEC,  "each", new var_fn_t( src_name, {}, {}, { .native = vec_each  }, 0, 0 ), false );
 
 	// get the type id for int iterable (register_type)
