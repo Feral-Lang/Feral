459ba77088d37e6345220a83820607e318e768a0
Electrux <ElectruxRedsworth@gmail.com>
2020-04-11 21:17:46 +0530

    updated build.sh to use system cores + use sudo only when required

--- build.sh
+++ build.sh
@@ -1,7 +1,36 @@
 #!/usr/bin/env sh
 echo '[INFO] Feral Installer - (c) Copyright 2020 Maximilian Götz (https://www.maxbits.net/)'
-echo '[INFO] Please make sure that Git, CMake, LibGMP and LibMPFR are installed!'
-echo '[INFO] Installation started. Checking for Git, sudo and CMake...'
+echo '[INFO] Please make sure that Sudo, Git, CMake, LibGMP and LibMPFR are installed!'
+echo '[INFO] Installation started. Fetching system cores...'
+
+# Get current working directory
+CWD="$(pwd)"
+
+function exit_handler {
+    echo '[INFO] Gracefully exiting.'
+    rm -rf ~/.feral-installer
+    cd $CWD
+}
+
+# Get system cores for faster build
+SYSNAME=$(uname)
+
+CORES=1
+if [ "$SYSNAME" == "Linux" ]; then
+    CORES=$(nproc)
+else # for BSD and macOS
+    CORES=$(sysctl -n hw.ncpu)
+fi
+echo "[INFO] Using $CORES cores"
+
+echo '[INFO] Checking for Git, Sudo and CMake...'
+# No sudo on macOS since /usr/local is not usually root owned (as long as homebrew is installed)
+SUDO="sudo"
+if [ "$SYSNAME" == "Darwin" ] && hash brew 2>/dev/null || [ "$(id -u)" == "0" ]; then
+    SUDO=""
+else
+    echo '[INFO] Using sudo.'
+fi
 
 # Check for Git
 if hash git 2>/dev/null; then
@@ -12,11 +41,13 @@ else
 fi
 
 # Check for sudo
-if hash sudo 2>/dev/null; then
-    echo '[INFO] sudo found.'
-else
-    echo '[FATAL] sudo not available! Install it to continue! Aborting...'
-    exit
+if [ "$sudo" == "sudo" ]; then
+    if hash sudo 2>/dev/null; then
+        echo '[INFO] sudo found.'
+    else
+        echo '[FATAL] sudo not available! Install it to continue! Aborting...'
+        exit
+    fi
 fi
 
 # Check for CMake
@@ -26,6 +57,10 @@ else
     echo '[FATAL] CMake not available! Install it to continue! Aborting...'
     exit
 fi
+
+# Trap SIGINT so that if program is ctrl-c'd while being in ~/.feral-installer,
+# we can delete that directory get back to the original directory
+trap exit_handler INT
 
 # Create a temporary directory
 echo '[INFO] Creating temporary directory at ~/.feral-installer...'
@@ -39,7 +74,7 @@ git clone https://github.com/Feral-Lang/Feral-Std.git
 build() {
     mkdir build && cd build
     cmake ..
-    sudo make install
+    $SUDO make -j$CORES install
     cd ..
 }
 
@@ -55,3 +90,5 @@ cd ../..
 
 echo '[INFO] Done! Cleaning up...'
 rm -rf ~/.feral-installer
+
+cd $CWD
