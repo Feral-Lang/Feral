f69fe21c21ae9f7cd2764ff287a5013afb05a91a
Maximilian Götz <contact@maxbits.net>
2020-04-11 14:33:22 +0200

    + Added build script + Updated docs

---
+++ build.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env sh
+echo '[INFO] Feral Installer - (c) Copyright 2020 Maximilian Götz (https://www.maxbits.net/)'
+echo '[INFO] Please make sure that Git, CMake, LibGMP and LibMPFR are installed!'
+echo '[INFO] Installation started. Checking for Git...'
+
+# Check for Git
+if hash git 2>/dev/null; then
+    echo '[INFO] Git found.'
+else
+    echo '[FATAL] Git not available! Install it to continue! Aborting...'
+    exit
+fi
+
+# Create a temporary directory
+echo '[INFO] Creating temporary directory at ~/.feral-installer...'
+mkdir ~/.feral-installer && cd ~/.feral-installer
+
+# Clone the language source code
+echo '[INFO] Cloning required repositories...'
+git clone https://github.com/Feral-Lang/Feral.git
+git clone https://github.com/Feral-Lang/Feral-Std.git
+
+build() {
+    mkdir build && cd build
+    cmake ..
+    sudo make install
+    cd ..
+}
+
+echo '[INFO] Building language...'
+cd Feral
+build
+cd ..
+
+echo '[INFO] Building standard library...'
+cd Feral-Std
+build
+cd ../..
+
+echo '[INFO] Done! Cleaning up...'
+rm -rf ~/.feral-installer
