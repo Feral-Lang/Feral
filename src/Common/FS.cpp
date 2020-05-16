f4e082b6a27df6655e24a7e9d5694fcb32e67525
Electrux <ElectruxRedsworth@gmail.com>
2020-05-16 20:05:51 +0530

    fix bug in fs::abs_path() logic

--- src/Common/FS.cpp
+++ src/Common/FS.cpp
@@ -27,14 +27,15 @@ bool exists( const std::string & loc )
 std::string abs_path( const std::string & loc, std::string * dir, const bool & dir_add_double_dot )
 {
 	static char abs[ MAX_PATH_CHARS ];
+	static char abs_tmp[ MAX_PATH_CHARS ];
 	realpath( loc.c_str(), abs );
 	if( dir != nullptr ) {
 		std::string _abs = abs;
 		* dir = _abs.substr( 0, _abs.find_last_of( '/' ) );
 		if( dir_add_double_dot ) {
 			* dir += "/..";
-			realpath( dir->c_str(), abs );
-			* dir = abs;
+			realpath( dir->c_str(), abs_tmp );
+			* dir = abs_tmp;
 		}
 	}
 	return abs;
