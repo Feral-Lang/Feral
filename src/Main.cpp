c5fdedb2813fde17820442d51e8fac5ab8e74660
Chirag-Khandelwal <github@mail.chiragkh.me>
2023-08-11 21:24:40 -0700

    added -d (dry run) cli argument

--- src/Main.cpp
+++ src/Main.cpp
@@ -20,7 +20,8 @@ int main(int argc, char **argv)
 	args.add("parse").setShort("p").setHelp("shows AST");
 	args.add("optparse").setShort("P").setHelp("shows optimized AST (AST after passes)");
 	args.add("ir").setShort("i").setHelp("shows codegen IR");
-	args.add("testdir").setShort("t").setHelp("test all feral programs in a directory");
+	args.add("testdir").setShort("t").setHelp("run all test cases in a directory");
+	args.add("dry").setShort("d").setHelp("dry run - generate IR but don't run the VM");
 	args.add("verbose").setShort("V").setHelp("show verbose compiler output");
 	if(!args.parse()) return 1;
 
