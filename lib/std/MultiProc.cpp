98c39ea58f54938f6587f133aa835a363205ad61
Chirag-Khandelwal <github@mail.chiragkh.me>
2024-08-04 22:57:18 -0700

    Moved {MultiProc,Ptr,Regex}Type.hpp -> {...}.hpp; Merged the same {...}Type.cpp files into the {...}.cpp files

--- lib/std/MultiProc.cpp
+++ lib/std/MultiProc.cpp
@@ -1,4 +1,5 @@
-#include "std/MultiProcType.hpp"
+#include "std/MultiProc.hpp"
+
 #include "VM/Interpreter.hpp"
 
 #if defined(FER_OS_WINDOWS)
@@ -12,6 +13,39 @@
 
 namespace fer
 {
+
+static size_t threadId = 0;
+
+VarMultiProc::VarMultiProc(const ModuleLoc *loc, Thread *thread, SharedFuture<int> *res, bool owner)
+	: Var(loc, false, false), thread(thread), res(res), id(threadId++), owner(owner)
+{}
+VarMultiProc::VarMultiProc(const ModuleLoc *loc, Thread *thread, SharedFuture<int> *res, size_t id,
+			   bool owner)
+	: Var(loc, false, false), thread(thread), res(res), id(id), owner(owner)
+{}
+VarMultiProc::~VarMultiProc()
+{
+	if(owner) {
+		if(thread != nullptr) {
+			thread->join();
+			delete thread;
+		}
+		if(res != nullptr) delete res;
+	}
+}
+
+Var *VarMultiProc::copyImpl(const ModuleLoc *loc)
+{
+	return new VarMultiProc(loc, thread, res, id, false);
+}
+void VarMultiProc::set(Var *from)
+{
+	VarMultiProc *t = as<VarMultiProc>(from);
+	owner		= false;
+	thread		= t->thread;
+	res		= t->res;
+	id		= t->id;
+}
 
 int execCommand(const String &cmd);
 
--- lib/std/Ptr.cpp
+++ lib/std/Ptr.cpp
@@ -1,8 +1,27 @@
-#include "PtrType.hpp"
+#include "Ptr.hpp"
+
 #include "VM/Interpreter.hpp"
 
 namespace fer
 {
+
+VarPtr::VarPtr(const ModuleLoc *loc, Var *val) : Var(loc, false, false), val(val) { incref(val); }
+VarPtr::~VarPtr() { decref(val); }
+
+Var *VarPtr::copyImpl(const ModuleLoc *loc) { return new VarPtr(loc, val); }
+void VarPtr::set(Var *from)
+{
+	decref(val);
+	val = as<VarPtr>(from)->val;
+	incref(val);
+}
+
+void VarPtr::update(Var *with)
+{
+	decref(val);
+	val = with;
+	incref(val);
+}
 
 //////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////// Functions ////////////////////////////////////////////
