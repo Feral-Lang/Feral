fa806b3e946e0c3d209121568a620a739a1d40b1
Chirag-Khandelwal <github@mail.chiragkh.me>
2025-03-10 23:44:38 -0700

    Added nil-coalesce (??) operator support

--- src/Lexer.cpp
+++ src/Lexer.cpp
@@ -48,8 +48,10 @@ const char *TokStrs[_LAST] = {
 "*=",
 "/=",
 "%=",
-"**", // power
-"//", // root
+"**",	 // power
+"//",	 // root
+"\?\?",	 // nil-coalesce
+"\?\?=", // nil-coalesce-assn
 // Post/Pre Inc/Dec
 "x++",
 "++x",
@@ -118,78 +120,6 @@ const char *TokStrs[_LAST] = {
 };
 
 Tok::Tok(int tok) : val((TokType)tok) {}
-
-const char *Tok::getUnaryNoCharCStr() const
-{
-	switch(val) {
-	case XINC:
-	case INCX: return "++";
-	case XDEC:
-	case DECX: return "--";
-	case UADD: return "+";
-	case USUB: return "-";
-	case UAND: return "&";
-	case UMUL: return "*";
-	case LNOT: return "!";
-	case BNOT: return "~";
-	default: break;
-	}
-	return "";
-}
-
-const char *Tok::getOperCStr() const
-{
-	switch(val) {
-	case ASSN: return "__assn__";
-	// Arithmetic
-	case ADD: return "__add__";
-	case SUB: return "__sub__";
-	case MUL: return "__mul__";
-	case DIV: return "__div__";
-	case MOD: return "__mod__";
-	case ADD_ASSN: return "__add_assn__";
-	case SUB_ASSN: return "__sub_assn__";
-	case MUL_ASSN: return "__mul_assn__";
-	case DIV_ASSN: return "__div_assn__";
-	case MOD_ASSN: return "__mod_assn__";
-	// Post/Pre Inc/Dec
-	case XINC: return "__xinc__";
-	case INCX: return "__incx__";
-	case XDEC: return "__xdec__";
-	case DECX: return "__decx__";
-	// Unary
-	case UADD: return "__uadd__";
-	case USUB: return "__usub__";
-	// Logic
-	case LAND: return "__logand__";
-	case LOR: return "__logor__";
-	case LNOT: return "__lognot__";
-	// Comparison
-	case EQ: return "__eq__";
-	case LT: return "__lt__";
-	case GT: return "__gt__";
-	case LE: return "__le__";
-	case GE: return "__ge__";
-	case NE: return "__ne__";
-	// Bitwise
-	case BAND: return "__band__";
-	case BOR: return "__bor__";
-	case BNOT: return "__bnot__";
-	case BXOR: return "__bxor__";
-	case BAND_ASSN: return "__band_assn__";
-	case BOR_ASSN: return "__bor_assn__";
-	case BNOT_ASSN: return "__bnot_assn__";
-	case BXOR_ASSN: return "__bxor_assn__";
-	// Others
-	case LSHIFT: return "__lshift__";
-	case RSHIFT: return "__rshift__";
-	case LSHIFT_ASSN: return "__lshift_assn__";
-	case RSHIFT_ASSN: return "__rshift_assn__";
-	case SUBS: return "__subscr__";
-	default: break;
-	}
-	return "";
-}
 
 Lexeme::Lexeme(ModuleLoc loc) : loc(loc), tok(INVALID) {}
 Lexeme::Lexeme(ModuleLoc loc, TokType type) : loc(loc), tok(type) {}
@@ -725,7 +655,16 @@ TokType getOperator(ModuleId moduleId, StringRef data, size_t &i, size_t line, s
 			}
 		}
 		SET_OP_TYPE_BRK(DOT);
-	case '?': SET_OP_TYPE_BRK(QUEST);
+	case '?':
+		if(i < len - 1 && NEXT == '?') {
+			++i;
+			if(i < len - 1 && NEXT == '=') {
+				++i;
+				SET_OP_TYPE_BRK(NIL_COALESCE_ASSN);
+			}
+			SET_OP_TYPE_BRK(NIL_COALESCE);
+		}
+		SET_OP_TYPE_BRK(QUEST);
 	case ':': SET_OP_TYPE_BRK(COL);
 	case ',': SET_OP_TYPE_BRK(COMMA);
 	case ';': SET_OP_TYPE_BRK(COLS);
