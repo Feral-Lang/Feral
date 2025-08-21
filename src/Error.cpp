f905411edec5bea06eb2f8f8b9bdd6951d4de375
Chirag-Khandelwal <github@mail.chiragkh.me>
2025-08-20 20:13:12 -0700

    Minor fix in utils/testdir

--- src/Error.cpp
+++ src/Error.cpp
@@ -1,16 +1,3 @@
-/*
-	MIT License
-
-	Copyright (c) 2022 Scribe Language Repositories
-
-	Permission is hereby granted, free of charge, to any person obtaining a
-	copy of this software and associated documentation files (the "Software"), to
-	deal in the Software without restriction, including without limitation the
-	rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
-	sell copies of the Software, and to permit persons to whom the Software is
-	furnished to do so.
-*/
-
 #include "Error.hpp"
 
 namespace fer
