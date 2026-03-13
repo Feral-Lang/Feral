6e50cb240acd67501d12c0971e51a86e90e43d9b
Elec <elec@dev>
2026-03-13 16:16:57 -0700

    Added VirtualRegisters logic in parsing

---
+++ src/Parser/VirtualRegisters.cpp
@@ -0,0 +1,56 @@
+#include "AST/VirtualRegisters.hpp"
+
+namespace fer::ast
+{
+
+void VRegManager::pushFunc()
+{
+    data.push_back({});
+    blocks.push_back({});
+    maxCount.push_back(0);
+    pushName("self"); // self is permanent
+}
+size_t VRegManager::popFunc()
+{
+    size_t res = maxCount.back();
+    maxCount.pop_back();
+    blocks.pop_back();
+    data.pop_back();
+    return res;
+}
+
+void VRegManager::pushBlk()
+{
+    if(data.empty()) return;
+    blocks.back().push_back(data.back().size());
+}
+void VRegManager::popBlk()
+{
+    if(data.empty()) return;
+    while(data.back().size() > blocks.back().back()) data.back().pop_back();
+    blocks.back().pop_back();
+}
+
+bool VRegManager::pushName(StringRef name)
+{
+    if(data.empty()) return false;
+    data.back().push_back(name);
+    if(maxCount.back() < data.back().size()) ++maxCount.back();
+    return true;
+}
+
+size_t VRegManager::getIndex(StringRef name)
+{
+    if(data.empty()) return -1;
+    auto loc = std::find(data.back().begin(), data.back().end(), name);
+    if(loc == data.back().end()) return -1;
+    return loc - data.back().begin();
+}
+
+size_t VRegManager::getLastIndex()
+{
+    if(data.empty()) return -1;
+    return data.back().size() - 1;
+}
+
+} // namespace fer::ast
